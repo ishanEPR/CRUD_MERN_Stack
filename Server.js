bfcac7b42acfcb8ae424a44b76a509187ebc7d74
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-03 19:38:54 +0530

    updated project

--- Server.js
+++ Server.js
@@ -1,7 +1,22 @@
 const express=require('express');
+const dotenv=require("dotenv");
+const morgan = require('morgan');
+const bodyparser=require("body-parser");
+
 const app=express();
 
+dotenv.config({path:'config.env'})
+
 const PORT=process.env.PORT || 8080;
+
+//log requests
+app.use(morgan("tiny"));
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true}))
+
+//set view engine
+app.set("view engine","ejs")
 
 app.get('/',(req,res)=>{
     res.send("Crud Application");
