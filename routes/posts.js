ac2d1a24372471e3c47350a4dcc8d4411f19c749
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-04 11:55:00 +0530

    create add router using post method

---
+++ routes/posts.js
@@ -0,0 +1,25 @@
+const express=require('express');
+//import database schema
+const posts=require('../models/posts');
+
+
+const router=express.Router();
+//here we can use router, then can use htttp request
+
+///save post
+router.post('/post/save',(req,res)=>{
+    let newPost=new Posts(req.body);
+    newPost.save((err)=>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        }
+        return res.status(200).json({
+            success:"Posts saved successfully"
+        });
+    });
+
+});
+
+module.exports=router;
