be66526f8a658ed159821aba1fb5ec8e247a1561
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-04 22:02:12 +0530

    create select,update, delete for backend

--- routes/posts.js
+++ routes/posts.js
@@ -22,6 +22,59 @@ router.post('/post/save',(req,res)=>{
 
 });
 
+//get posts
+
+router.get('/posts',(req,res)=>{
+    Posts.find().exec((err,posts)=>{
+        if(err)
+        {
+            return res.status(400).json({
+                error:err
+            });
+        }
+       
+            return res.status(200).json({
+                success:true,
+                existingPosts:posts,
+            });
+       
+    });
+
+});
+
+
+//update post
+router.put('/post/update/:id',(req,res)=>{
+    Posts.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set:req.body
+        },
+        (err,post)=>{
+            if(err){
+                return res.status(400).json({error:err});
+            }
+            return res.status(200).json({
+                success:"Updated Succesfully"});
+        }
+    );
+});
+
+///delete posts
+
+router.delete('/post/delete/:id',(req,res)=>{
+    Posts.findOneAndRemove(req.params.id).exec((err,deletePost)=>{
+        if(err) return res.status(400).json({
+            message:"Delete Unsuccessful",err
+        });
+
+        return res.status(200).json({
+            message:"Delete Successful",
+            deletePost
+        });
+    });
+}); 
+
 
 
 module.exports=router;
