df08d9162dde8280ee64a92fc61f6f3723372a2c
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-04 11:40:33 +0530

    create database schema

--- Server.js
+++ Server.js
@@ -7,7 +7,10 @@ const PORT=8000;
 const DB_URL='mongodb+srv://ishan97:19970330@cluster0.dbi3m.mongodb.net/student_db?retryWrites=true&w=majority';
 
 //create database connection
-mongoose.connect(DB_URL).then(()=>{
+mongoose.connect(DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
     console.log('Database connection successfull');
 }).catch((err)=>{
     console.log('Database connection error');
