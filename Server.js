a423096d75c50e7f690fae2a0ffa395f75197fdd
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-04 11:29:27 +0530

    create database connection

--- Server.js
+++ Server.js
@@ -6,6 +6,13 @@ const app=express();
 const PORT=8000;
 const DB_URL='mongodb+srv://ishan97:19970330@cluster0.dbi3m.mongodb.net/student_db?retryWrites=true&w=majority';
 
+//create database connection
+mongoose.connect(DB_URL).then(()=>{
+    console.log('Database connection successfull');
+}).catch((err)=>{
+    console.log('Database connection error');
+})
+
 app.listen(PORT,()=>{
     console.log('App is running on '+PORT);
 }
