40d76fbc4b13373ab5efdbeecb762066c21973a9
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-07 15:58:09 +0530

    add create router dom

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';
+import CreatePost from './components/CreatePost';
+import EditPost from './components/EditPost';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import PostDetails from './components/PostDetails';
+
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+        <NavBar/>
+          <Route path="/" exact Component={Home}/>
+          <Route path="/add" Component={CreatePost}/>
+          <Route path="/edit/:id" Component={EditPost}/>
+          <Route path="/post/:id" Component={PostDetails}/>
+
+        
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class CreatePost extends Component {
+export default class NavBar extends Component {
     render() {
         return (
             <div>
-            CreatePost
+            NavBar
                 
             </div>
         )
