df05d029f870b7d5f8384cf4a1ae0b0e67003887
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-08 20:49:07 +0530

    updated create post

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,12 +1,79 @@
 import React, { Component } from 'react'
 
 export default class CreatePost extends Component {
+
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            topic:"",
+            description:"",
+            postCategory:""
+        }
+    }
+
+
+    handleInputChange=(e)=>{
+        const {name,value}=e.target;
+
+        this.setState({
+            ...this.state,
+            [name]:value
+        })
+    }
+
+    onSubmit=(e)=>{
+        e.preventDefault();
+
+        const {topic,description,postCategory}=this.state;
+
+        const data={
+            topic:topic,
+            description:description,
+            postCategory:postCategory
+        }
+        console.log(data);
+    }
     render() {
-        return (
-            <div>
-            CreatePost
-                
+        return(
+            <div className="container">
+                <form className="needs-validation" noValidate>
+                    <div className="form-group" style={{marginTop:'15px'}}>
+                        <label >Enter Topic</label>
+                        <input type="text" className="form-control" name="topic"  placeholder="Enter Topic"
+                        value={this.state.topic}
+                            onChange={this.handleInputChange
+    
+                                //setName(e.target.value);
+                            }
+                        />
+                        
+                    </div>
+    
+                    <div className="form-group">
+                        <label for="age">Enter Description</label>
+                        <input type="text" className="form-control" name="description"  placeholder="Enter Description"
+                        value={this.state.description}
+                            onChange={this.handleInputChange}
+                        />
+                        
+                    </div>
+    
+                    <div className="form-group">
+                        <label for="gender">Enter Post Category</label>
+                        <input type="text" className="form-control" id="gender"  placeholder="Enter Post Category" 
+                        value={this.state.postCategory}
+                        onChange={
+                            this.handleInputChange
+                        }/>
+                        
+                    </div>
+                   
+                    <button type="submit" class="btn btn-success" style={{marginTop:20}}
+                    onClick={this.onSubmit}
+                    >Submit</button>
+                </form>
             </div>
-        )
+        );
     }
 }
